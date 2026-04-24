4ef3e19103b865ffaeefc7c16dbc53a57d8caf95
Sahil Ansari <sa760887@gmail.com>
2026-04-25 05:02:12 +0530

    fix

--- lms/skillnetwork/settings.py
+++ lms/skillnetwork/settings.py
@@ -235,6 +235,16 @@ LOGGING = {
 }
 
 
+# Email Configuration
+EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+RECIPIENT_EMAIL = [EMAIL_HOST_USER]
+
 
 # NFT CertiChain - Blockchain Certificate Configuration
 EMAILJS_SERVICE_ID = env("EMAILJS_SERVICE_ID", default="")
